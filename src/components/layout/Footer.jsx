c4a334c0b9209d2c25d291c646e8b9c35a0a1447
unknown <lina083017@gmail.com>
2026-02-09 15:55:32 +0900

    초안

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,33 +1,19 @@
 import "./Footer.css";
 import logo from "../../assets/images/logo.png";
+import github from "../../assets/images/github.png";
+import notion from "../../assets/images/notion.png";
+import figma from "../../assets/images/figma.png";
+import zep from "../../assets/images/zep.png";
 
 const FOOTER_SECTIONS = {
-  "Use cases": [
-    "UI design",
-    "UX design",
-    "Wireframing",
-    "Diagramming",
-    "Brainstorming",
-    "Online whiteboard",
-    "Team collaboration",
-  ],
-  Explore: [
-    "Design",
-    "Prototyping",
-    "Development features",
-    "Design systems",
-    "Collaboration features",
-    "Design process",
-    "FigJam",
-  ],
-  Resources: [
-    "Blog",
-    "Best practices",
-    "Colors",
-    "Color wheel",
-    "Support",
-    "Developers",
-    "Resource library",
+  "Our Team": [
+    "오찬경",
+    "박명환",
+    "김가인",
+    "오현진",
+    "전건호",
+    "권도희",
+    "정소연",
   ],
 };
 
@@ -35,18 +21,17 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-inner">
-        {/* 브랜드 영역 */}
+
         <div className="footer-brand">
           <img src={logo} alt="logo" className="logo-img" />
           <div className="social-icons">
-            <span />
-            <span />
-            <span />
-            <span />
+            <img src={github} alt="github" />
+            <img src={figma} alt="figma" />
+            <img src={notion} alt="notion" />
+            <img src={zep} alt="zep" />
           </div>
         </div>
 
-        {/* 링크 섹션 */}
         {Object.entries(FOOTER_SECTIONS).map(([title, items]) => (
           <div key={title} className="footer-section">
             <h4>{title}</h4>
