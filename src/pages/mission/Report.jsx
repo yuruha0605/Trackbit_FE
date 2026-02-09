16b896f9962fb7c5f5f410957847f26d778062e9
ohyoenjin04 <ohyeonjin85@gmail.com>
2026-02-09 18:25:24 +0900

    Update Report.jsx

--- src/pages/mission/Report.jsx
+++ src/pages/mission/Report.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../Report.css';
+import './Report.css';
 
 const Report = () => {
   // 데이터 예시
@@ -71,3 +71,4 @@ const Report = () => {
 
 
 export default Report;
+
