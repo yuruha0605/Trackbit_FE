60a992ac69456cf36235d912dc8e64ce8f9efcf3
unknown <lina083017@gmail.com>
2026-02-11 18:26:36 +0900

    유저 정보(이름) 갱신 시 헤더 갱신 안되던 문제 수정

--- src/pages/user/SignIn.jsx
+++ src/pages/user/SignIn.jsx
@@ -8,7 +8,7 @@ function SignIn() {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const from = location.state?.from?.pathname || "/"
+  const from = location.state?.from?.pathname || "/mypage"
 
   const { login } = useAuth();
 
